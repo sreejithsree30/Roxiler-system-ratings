3698259272b1806e93a45aee892ee038d5ecdb7c
SREEJITH S S <125574676+sreejithsree30@users.noreply.github.com>
2025-07-12 07:49:39 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://roxiler-system-ratings-6s3p.vercel.app/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
